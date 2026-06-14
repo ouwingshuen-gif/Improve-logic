ade92b6da1b1f1ee9e1e6e518e4560a4144db674
ouwingshuen-gif <ouwingshuen@gmail.com>
2026-06-15 03:32:24 +0800

    Add files via upload

---
+++ new/gemini-code-1781465481629.js
@@ -0,0 +1,11 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA({
+  // Next.js 其他配置
+});
